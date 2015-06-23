e562aaf782b0c1194804c77e22d6512df243f1b8
Sebastien Cail <scail@proginov.com>
2015-06-23 21:18:42 +0200

    cope chains

---
+++ scope-chains-closures/scope-chains2.js
@@ -0,0 +1,8 @@
+
+function foo() {
+	function zip() {
+		var quux;
+	}
+	quux = "aaa";
+	var bar;
+}
---
+++ scope-chains-closures/scope-closure.js
@@ -0,0 +1,11 @@
+
+function foo() {
+	var bar;
+	function zip() {
+		var quux;
+		bar = true;
+	}
+
+	quux = "aaa";
+	return zip;
+}
